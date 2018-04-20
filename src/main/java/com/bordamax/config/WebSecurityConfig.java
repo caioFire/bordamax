3b28ce6b12b3b15f7dc089e5f8442d7ca8c379fc
Caio <caio@getconsultoria.com.br>
2018-04-19 22:44:28 -0300

    tomcat

--- src/main/java/com/bordamax/config/WebSecurityConfig.java
+++ src/main/java/com/bordamax/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.bordamax.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 /**
@@ -29,23 +35,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
             .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/amostra/**").hasAnyRole("ADMIN", "USER")
-               // .antMatchers(HttpMethod.GET, "/amostra/**").permitAll()
 
-                .antMatchers(HttpMethod.GET, "/cliente/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/localizacao/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/login/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/home/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.GET, "/amostra/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.GET, "/cliente/**").hasAnyRole("ROLE_ADMIN", "USER")
+//                .antMatchers(HttpMethod.GET, "/localizacao/**").hasAnyRole("ADMIN","USER")
+//                .antMatchers(HttpMethod.GET, "/login/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.GET, "/home/**").hasAnyRole("ADMIN", "USER")
+//
+//                .antMatchers(HttpMethod.POST, "/cliente/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.POST, "/amostra/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.POST, "/localizacao/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.POST, "/login/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.POST, "/home/**").hasAnyRole("ADMIN", "USER")
 
-                .antMatchers(HttpMethod.POST, "/cliente/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.POST, "/amostra/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.POST, "/localizacao/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.POST, "/login/**").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.POST, "/home/**").hasAnyRole("ADMIN", "USER")
+//                .antMatchers(HttpMethod.GET, "/usuario/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST, "/usuario/**").hasRole("ADMIN")
 
-                .antMatchers(HttpMethod.GET, "/usuario/getIndicador").hasAnyRole("ADMIN", "USER")
-                .antMatchers(HttpMethod.GET, "/usuario/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/usuario/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/**").permitAll()
+                .antMatchers(HttpMethod.PUT, "/**").permitAll()
+                .antMatchers(HttpMethod.DELETE, "/**").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+
                 .antMatchers("/css/**", "/js/**", "/webjars/**","/resources/**", "/**").permitAll()
                 .anyRequest()
                 .authenticated()
@@ -62,7 +74,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
             .and()
                 .csrf()
-                .disable().authorizeRequests();
+                .disable().authorizeRequests()
+            .and()
+                .cors();
 
     }
 
@@ -71,7 +85,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(custonUsuarioDetailService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://13.58.31.125:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+        configuration.setAllowedHeaders(Arrays.asList("*", ""));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
 
 
